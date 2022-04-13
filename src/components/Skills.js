4d5e43dc97bef92de5338e901cf1a7f1e87ab189
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-04-13 16:51:37 +0900

    feat: skill 추가
    skill 컴포넌트 추가, mydata.js 추가, mydata의 스킬들을 skill 컴포넌트에 뿌림
    Skills.scss 추가

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,22 @@
 import React from "react"
+import data from "./myData";
 
 const Skills = () =>{
     return (
-        <div style={{backgroundColor:'orange'}}>
+        <div className="section">
+            <div className="container">
+                <div className="skills-container">
+                    <h1>Skills</h1>
+                    <div className="skills-grid">
+                        {data.skills.map((skill,index)=>(
+                            <div className="skill" key={index}>
+                                <img src={skill.img} alt="css"></img>
+                                <p>{skill.para}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/myData.js
@@ -0,0 +1,37 @@
+
+import html from "../img/html.png";
+import css from "../img/css.png";
+import java from "../img/java.png";
+import js from "../img/js.png";
+import mysql from "../img/mysql.png";
+import python from "../img/python.png";
+
+export default{
+    skills:[
+        {
+            img:java,
+            para: "java 설명"
+        },
+        {
+            img:mysql,
+            para: "mysql 설명"
+        },
+        {
+            img:python,
+            para: "python 설명"
+        },
+        {
+            img: html,
+            para: "html 설명"
+        },
+        {
+            img:css,
+            para: "css 설명"
+        },
+        {
+            img:js,
+            para: "js 설명 wmlfkmawefmawmekfawwf"
+        },
+        
+    ]
+}
